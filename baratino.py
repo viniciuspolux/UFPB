946e1b7eaa4fc7aaf33e8da08a29d22e5c30df31
Polux <30605184+viniciuspolux@users.noreply.github.com>
2017-08-24 19:25:32 -0300

    Listas de IP

---
+++ baratino.py
@@ -0,0 +1,17 @@
+atilla = input("Atilla vai pra festa ? ")
+polux = input("Polux vai pra festa ? ")
+igor = input("Igor vai pra festa ? ")
+
+if (atilla == "sim") and (polux == "sim") and (igor == "sim"):
+    print("BARATINEIRME EIRME")
+else:
+    print("Festa tranquila, tudo nos conformes")
+    
+gota = input("Vai ter gota patrocinada por thales ? ")
+
+if (gota == "sim"):
+    print("Thales e boazinha ficarão uma arara")
+else:
+    print("Tudo sob controle")
+
+
